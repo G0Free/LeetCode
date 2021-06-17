e9c6c653af5e81dfcc3b4cc95c39c86e0162f9bd
G0Free <gyorfi.bence@gmail.com>
2021-06-17 18:48:48 +0200

    Image Rotating Problem added.

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -10,12 +10,20 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            int[] inputArray = { 1, 3, 5, 6 };
+            /*int[] inputArray = { 1, 3, 5, 6 };
             int target = 7;
 
             int output = 0;
             output = SearchInsertProblem.SearchInsert(inputArray, target);
-            Console.WriteLine(output);
+            Console.WriteLine(output);*/
+            int[] a = { 1, 2, 3 };
+            int[] b = { 4, 5, 6 };
+            int[] c = { 7, 8, 9 };
+            int[][]matrix = { a, b, c};
+            RotateImageProblem.Kiir(matrix);
+            RotateImageProblem.Rotate(ref matrix);
+            Console.WriteLine();
+            RotateImageProblem.Kiir(matrix);
 
             Console.ReadLine();
 
--- LeetCode/RotateImageProblem.cs
+++ LeetCode/RotateImageProblem.cs
@@ -8,6 +8,72 @@ namespace LeetCode
 {
     class RotateImageProblem
     {
+        public static void Rotate(ref int[][] matrix)
+        {
+            ;
+            int resetNumber = (matrix.GetLength(0) - 1);
+            int stepNumber = resetNumber;
+
+            int i = 0;
+            int j = 0;
+            int runnerX = i;
+            int runnerY = j;
+            int save = matrix[i][j];
+            int beirando = matrix[i][j];
+            int ctr = 0;
+
+            int starterI = -1;
+            int starterJ = -1;
+
+            while (i != starterI || j != starterJ)
+            {
+                starterI = i;
+                starterJ = j;
+                //beirando = matrix[i,j];
+                beirando = save;
+                //search
+                while (runnerX < matrix.GetLength(0) && 0 < stepNumber)
+                {
+                    runnerX++;
+                    stepNumber--;
+                    if (runnerX == matrix.GetLength(0))
+                    {
+                        runnerX--;
+                    }
+                }
+                while (runnerY < matrix.GetLength(1) && 0 < stepNumber)
+                {
+                    runnerY++;
+                    stepNumber--;
+                    if (runnerY == matrix.GetLength(1))
+                    {
+                        runnerY--;
+                    }
+                }
+                //end of search
+
+                save = matrix[runnerX][runnerY];
+                //matrix[runnerX,runnerY] = matrix[i,j];
+                matrix[runnerX][runnerY] = beirando;
+                i = runnerX;
+                j = runnerY;
+                stepNumber = resetNumber;
+                //ctr++;
+            }
+            ;
+        }
+
+        public static void Kiir(int[][] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    Console.Write(matrix[i][j] + "\t");
+                }
+                Console.WriteLine("\n");
+            }
+        }
 
     }
 }
